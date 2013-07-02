345ff1c9cfb56559750d70d1a3bdcb33e0ea0690
johngallagher <john@synapticmishap.co.uk>
2013-07-02 13:13:21 +0100

    Added url.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,5 @@
 class Entry < ActiveRecord::Base
-  attr_accessible :end, :project_id, :start
+  attr_accessible :end, :project_id, :start, :url
 
   belongs_to :project
 end
--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -3,5 +3,6 @@ require 'spec_helper'
 describe Entry do
   it { should respond_to :started_at }
   it { should respond_to :finished_at }
+  it { should respond_to :url }
   it { should belong_to :project }
 end
