d414ae0546acaba4d4dd6c596f9e31f8c07fb31e
John Gallagher <john@synapticmishap.co.uk>
2013-07-09 22:45:41 +0100

    Same url.

--- spec/lib/trainer_spec.rb
+++ spec/lib/trainer_spec.rb
@@ -8,11 +8,20 @@ describe Trainer do
   describe ".train" do
     When { Trainer.new(entry, video).train }
 
-    Then { entry.should be_trained_for_project(video) }
-    Then { Rule.should have(1).items }
+    context "blank slate" do
+      Then { entry.should be_trained_for_project(video) }
+      Then { Rule.should have(1).items }
+    end
 
-    context "trained for newsroom" do
+    context "same url trained for newsroom" do
       Given { FactoryGirl.create(:rule, url: entry.url, project: newsroom) }
+
+      Then { entry.should be_trained_for_project(video) }
+      Then { Rule.should have(1).items }
+    end
+
+    context "different entry, same url" do
+      Given(:entry_2) { FactoryGirl.create(:entry) }
 
       Then { entry.should be_trained_for_project(video) }
       Then { Rule.should have(1).items }
