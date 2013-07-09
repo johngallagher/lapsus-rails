4d300ecd78cd3619f138f3b7fbe16cc3b4f4a866
John Gallagher <john@synapticmishap.co.uk>
2013-07-09 02:48:28 +0100

    Moved out common initialisers.

--- spec/lib/trainer_spec.rb
+++ spec/lib/trainer_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 describe Trainer do
-  context "no previous rules" do
-    Given { Rule.destroy_all}
-    Given!(:entry)   { FactoryGirl.create(:entry) }
-    Given!(:video_project) { FactoryGirl.create(:project, name: "Video") }
+  Given { Rule.destroy_all}
+  Given(:entry)   { FactoryGirl.create(:entry) }
+  Given(:video_project) { FactoryGirl.create(:project, name: "Video") }
+  Given(:newsroom_project) { FactoryGirl.create(:project, name: "Newsroom") }
 
+  context "no previous rules" do
     When { Trainer.new(entry, video_project).train }
 
     Then { entry.should be_trained }
@@ -14,11 +15,7 @@ describe Trainer do
     Then { Rule.first.url.should == entry.url }    
   end
 
-  context "url has been trained for newsroom" do
-    Given { Rule.destroy_all}
-    Given!(:entry)            { FactoryGirl.create(:entry) }
-    Given!(:video_project)    { FactoryGirl.create(:project, name: "Video") }
-    Given!(:newsroom_project) { FactoryGirl.create(:project, name: "Newsroom") }
+  context "entry has already been trained for newsroom" do
     Given!(:newsroom_rule)    { FactoryGirl.create(:rule, url: entry.url, project: newsroom_project) }
     
     When { Trainer.new(entry, video_project).train }
