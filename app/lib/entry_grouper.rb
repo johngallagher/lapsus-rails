dfec3b6ccf4761a685784f3037266cc24c02d3f7
John Gallagher <john@synapticmishap.co.uk>
2014-11-24 20:30:52 +0000

    Remove entry grouper - not needed now.

--- app/lib/entry_grouper.rb
+++
@@ -1,9 +0,0 @@
-class EntryGrouper
-  def group
-    if Rule.all.empty?
-      Group.create(entries: Entry.all, url: Entry.first.url)
-    else
-      Group.create(entries: Entry.all, url: Rule.first.url)
-    end
-  end  
-end
