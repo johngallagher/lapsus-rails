b92503425701fc685932501afe742a547732eed5
John Gallagher <john@synapticmishap.co.uk>
2014-11-24 20:51:59 +0000

    Reinstall Rails 4.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Prototype::Application.config.secret_token = 'e75862c7939450ed381ebcfc0886719a527263c1faeb54e4c88de3e6065fbdc8c8b1bab74f04c5a19bbd8d3364feb4637ddb196dee1526e6b0cdac9c200f09ab'
