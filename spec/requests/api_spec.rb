3cc1cff102870b3388ff330cf218b0a94aeb513c
John Gallagher <john@synapticmishap.co.uk>
2014-12-08 16:32:36 +0000

    Simplified test.

--- spec/requests/api_spec.rb
+++ spec/requests/api_spec.rb
@@ -4,12 +4,12 @@ require 'pry'
 describe 'lapsus api', type: :request do
   describe 'POST /api/v1/entries' do
     it 'with no containers it creates an entry with no project' do
-      post '/api/v1/entries', { entries: [rails_gemfile_from_client]}, format: :json
+      post '/api/v1/entries', { entries: [rails_gemfile_document]}, format: :json
 
       expect(Entry.count).to eq(1)
-      expect(Entry.first.started_at).to eq(rails_gemfile.started_at)
-      expect(Entry.first.finished_at).to eq(rails_gemfile.finished_at)
-      expect(Entry.first.url).to eq('/Users/John/Code/rails/Gemfile')
+      expect(Entry.first.started_at).to eq(rails_gemfile_document[:started_at])
+      expect(Entry.first.finished_at).to eq(rails_gemfile_document[:finished_at])
+      expect(Entry.first.url).to eq(rails_gemfile_document[:url])
 
       expect(Entry.first.project).to be_nil
     end
@@ -17,12 +17,12 @@ describe 'lapsus api', type: :request do
     it 'with a project that matches the document creates an entry with project' do
       FactoryGirl.create(:container, url: '/Users/John/Code')
 
-      post '/api/v1/entries', { entries: [rails_gemfile_from_client]}, format: :json
+      post '/api/v1/entries', { entries: [rails_gemfile_document]}, format: :json
 
       expect(Entry.count).to eq(1)
-      expect(Entry.first.started_at).to eq(rails_gemfile.started_at)
-      expect(Entry.first.finished_at).to eq(rails_gemfile.finished_at)
-      expect(Entry.first.url).to eq('/Users/John/Code/rails/Gemfile')
+      expect(Entry.first.started_at).to eq(rails_gemfile_document[:started_at])
+      expect(Entry.first.finished_at).to eq(rails_gemfile_document[:finished_at])
+      expect(Entry.first.url).to eq(rails_gemfile_document[:url])
 
       expect(Entry.first.project).to be_present
       expect(Entry.first.project.name).to eq('rails')
@@ -31,22 +31,10 @@ describe 'lapsus api', type: :request do
   end
 end
 
-def rails_gemfile
-  FactoryGirl.build(:entry, :rails_gemfile)
-end
-
-def rails_gemfile_from_client
-  rails_gemfile.attributes.symbolize_keys.slice(:started_at, :finished_at, :url)
-end
-
-def document_url
-  '/Users/John/Documents/hello.txt'
-end
-
-def start_time
-  DateTime.parse('2014-01-01 9:11:22')
-end
-
-def end_time
-  DateTime.parse('2014-01-01 10:22:22')
+def rails_gemfile_document
+  {     
+    started_at: "2013-07-01 18:23:47",
+    finished_at: "2013-07-01 18:23:47",
+    url: "/Users/John/Code/rails/Gemfile"
+  }
 end
