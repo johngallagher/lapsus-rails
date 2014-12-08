c9326a522cfcdfd33f4e1cecd9aa9159c4f387e9
John Gallagher <john@synapticmishap.co.uk>
2014-12-08 20:08:10 +0000

    Move all training into the trainer.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,19 +1,6 @@
 class Entry < ActiveRecord::Base
   belongs_to :project
 
-  def self.create_with_project(attrs)
-    entry = Entry.create(attrs)
-
-    Container.all.each do |container|
-      if container.contains_project_for_entry?(entry)
-        entry.project = Project.find_or_create_from_container_and_entry(container, entry)
-        entry.save!
-      end
-    end
-    entry
-  end
-
-
   def path_depth
     path_components.count
   end
--- spec/models/trainer_spec.rb
+++ spec/models/trainer_spec.rb
@@ -9,6 +9,38 @@ describe Trainer do
     expect(Entry.first.project.url).to eq('/Users/John/Code/rails')
     expect(Entry.first.project.name).to eq('rails')
   end
+
+  it 'when file is deeply nested within a project directory in the container it assigns a project' do
+    assuming_an_entry_with_url('/Users/John/Code/rails/lib/rails/main.rb')
+    assuming_a_container('/Users/John/Code')
+    Trainer.train
+    expect(Entry.first.project).to be_present
+    expect(Entry.first.project.url).to eq('/Users/John/Code/rails')
+    expect(Entry.first.project.name).to eq('rails')
+  end
+
+  it 'when file is within a project directory in the container it assigns a project' do
+    assuming_an_entry_with_url('/Users/John/Code/rails/Gemfile')
+    assuming_a_container('/Users/John/Code')
+    Trainer.train
+    expect(Entry.first.project).to be_present
+    expect(Entry.first.project.url).to eq('/Users/John/Code/rails')
+    expect(Entry.first.project.name).to eq('rails')
+  end
+
+  it 'when file is in the container it doesnt assign any project' do
+    assuming_an_entry_with_url('/Users/John/Code/README.md')
+    assuming_a_container('/Users/John/Code')
+    Trainer.train
+    expect(Entry.first.project).to be_nil
+  end
+
+  it 'when entry is outside a container it doesnt assign it to a project' do
+    assuming_an_entry_with_url('/Users/John/.vimrc')
+    assuming_a_container('/Users/John/Code')
+    Trainer.train
+    expect(Entry.first.project).to be_nil
+  end
 end
 
 def assuming_an_entry_with_url(url)
