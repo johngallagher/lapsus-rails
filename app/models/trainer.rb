f03f806cea6487d1c30524a08ee17378f4ede647
John Gallagher <john@synapticmishap.co.uk>
2014-12-09 00:59:45 +0000

    Hack to make sure projects are updated correctly.

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,11 +1,11 @@
 class Trainer
   def self.train
+    Project.delete_all
+    Entry.all.each { |entry| entry.project = nil; entry.save! }
     Container.all.each do |container|
       Entry.all.each do |entry|
         if container.contains_project_for_entry?(entry)
           entry.project = Project.find_or_create_from_container_and_entry(container, entry)
-        else
-          entry.project = nil
         end
         entry.save!
       end
