826b21f8dda57057a944ecb7a292dd1ad51b9f53
John Gallagher <john@synapticmishap.co.uk>
2014-12-09 01:00:19 +0000

    Train after creating a controller.

--- app/controllers/containers_controller.rb
+++ app/controllers/containers_controller.rb
@@ -1,32 +1,26 @@
 class ContainersController < ApplicationController
   def new
-    @container = Container.new(container_params)
+    @container = Container.new(params.permit(:container))
     render :new
   end
 
   def create
     @container = Container.new(container_params)
     if @container.save
-      redirect_to entries_url
+      Trainer.train
+      flash[:alert] = @container.errors
+      redirect_to projects_url
     else
       render :new
     end
   end
 
-  def edit
-  end
-
-  def update
-  end
-
-  def delete
-  end
-
-  def destroy
+  def index
+    @containers = Container.all
   end
 
   private
   def container_params
-    params.permit(:name, :url)
+    params.require(:container).permit(:name, :url)
   end
 end
