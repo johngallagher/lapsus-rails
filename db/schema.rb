0e12fe0dea6e9870c0267ad32c204faff5f9848c
John Gallagher <john@synapticmishap.co.uk>
2014-12-11 07:12:27 +0000

    Revert "Install postgres for heroku."
    This reverts commit 3d55a54d111b894abdabc02576cc2dd54183049b.

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20141209002142) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "containers", force: true do |t|
     t.string "name"
     t.string "url"
