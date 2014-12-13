a1614421de3c94c99aa8731875e7649aac3100c5
John Gallagher <john@synapticmishap.co.uk>
2014-12-13 02:20:20 +0000

    Readable time in words for projects.

--- .scratchpad.rb
+++ .scratchpad.rb
@@ -1,9 +1,11 @@
-require 'date' # => true
+require 'oauth2'
 
+id = '9a9d96c5208e8ab44c8e29cafbb979feb2518a21db4ac82c127e69a34754c24a'
+secret = '9fa862ba98cb14a8637307c7a92a6559f701503e441d4d927cee5e93de7bbd91'
+client = OAuth2::Client.new(id, secret, site: 'http://localhost:3000')
+access_token = client.password.get_token('john@jg.com', 'password1')
+access_token.token # => "723b315d10fb3b23d9b38fd025f598ad5cdbd9b6f338e8d9ab9baf198d73f66a"
 
-DateTime.parse('2014-11-30 13:54:47') # => #<DateTime: 2014-11-30T13:54:47+00:00 ((2456992j,50087s,0n),+0s,2299161j)>
-
-require 'yaml'
-{ wow: 'hello'}.to_yaml # => "---\n:wow: hello\n"
-
-File.ftype('/Users/johngallagher/Dropbox/Projects/ProgrammingProjects/CurrentProjects/lapsus-rails') # => "directory"
+#client = OAuth2::Client.new('the_client_id', 'the_client_secret', :site => "http://example.com")
+#access_token = client.password.get_token('user@example.com', 'sekret')
+#puts access_token.token
