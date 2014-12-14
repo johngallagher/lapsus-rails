5de41781258d48a6e35aae403f7e6f9cdc9383ad
John Gallagher <john@synapticmishap.co.uk>
2014-12-14 17:37:18 +0000

    Reinstitute the api specs.

--- spec/requests/api_spec.rb
+++ spec/requests/api_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
 
-describe 'lapsus api', type: :request, vcr: true do
+describe 'lapsus api', type: :request do
+  before(:each) do
+    @user = FactoryGirl.create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:doorkeeper_token) { double(resource_owner_id: @user.id, acceptable?: true) }
+  end
+
   describe 'POST /api/v1/entries' do
     it 'with no containers it creates an entry with no project' do
-      user = FactoryGirl.create(:user)
-      allow_any_instance_of(ApplicationController).to receive(:doorkeeper_token) { double(resource_owner_id: user.id, acceptable?: true) }
       post '/api/v1/entries', { entries: [rails_gemfile_document]}, format: :json
 
       expect(response).to have_http_status(:created)
@@ -12,12 +15,13 @@ describe 'lapsus api', type: :request, vcr: true do
       expect(Entry.first.started_at).to eq(rails_gemfile_document[:started_at])
       expect(Entry.first.finished_at).to eq(rails_gemfile_document[:finished_at])
       expect(Entry.first.path).to eq(rails_gemfile_document[:path])
+      expect(Entry.first.user_id).to eq(@user.id)
 
       expect(Entry.first.project).to be_nil
     end
 
-    xit 'with a project that matches the document creates an entry with project' do
-      FactoryGirl.create(:container, path: '/Users/John/Code')
+    it 'with a project that matches the document creates an entry with project' do
+      FactoryGirl.create(:container, path: '/Users/John/Code', user_id: @user.id)
 
       post '/api/v1/entries', { entries: [rails_gemfile_document]}, format: :json
 
@@ -25,15 +29,16 @@ describe 'lapsus api', type: :request, vcr: true do
       expect(Entry.first.started_at).to eq(rails_gemfile_document[:started_at])
       expect(Entry.first.finished_at).to eq(rails_gemfile_document[:finished_at])
       expect(Entry.first.path).to eq(rails_gemfile_document[:path])
+      expect(Entry.first.user_id).to eq(@user.id)
 
       expect(Entry.first.project).to be_present
       expect(Entry.first.project.name).to eq('rails')
       expect(Entry.first.project.path).to eq('/Users/John/Code/rails')
     end
 
-    xit 'with an invalid path it returns errors' do
+    it 'with a missing path it returns errors' do
       post '/api/v1/entries', { entries: [{ started_at: "2013-01-01 18:00:00", finished_at: "2013-01-01 19:00:00" }]}, format: :json
-      expect(response.status).to eq(422)
+      expect(response).to have_http_status(:unprocessable_entity)
       expect(JSON(response.body).first['errors']).to eq(["Path can't be blank"])
     end
   end
