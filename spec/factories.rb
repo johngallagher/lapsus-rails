d80b1cee4642f7083bf0841acfd7442f38b5ef3c
John Gallagher <john@synapticmishap.co.uk>
2014-12-14 17:57:09 +0000

    Don't set user id in factories - this breaks the tests for some reason.

--- spec/factories.rb
+++ spec/factories.rb
@@ -8,19 +8,16 @@ FactoryGirl.define do
   factory :container do
     name "Code"
     path "/Users/John/Code"
-    user_id User.first ? User.first.id : nil
   end
 
   factory :project do
     name "Code"
     path "/Users/John/Code/rails"
-    user_id User.first ? User.first.id : nil
   end
 
   factory :entry do
     sequence(:started_at, 10) { |seconds|  "2013-07-01 18:23:#{seconds}" }
     sequence(:finished_at, 11) { |seconds|  "2013-07-01 18:23:#{seconds}" }
     path "/Users/John/Code/rails/Gemfile"
-    user_id User.first ? User.first.id : nil
   end
 end
