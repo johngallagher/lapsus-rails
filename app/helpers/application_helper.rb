08fcd07d583f708ea02c0b6468792ec435a65415
John Gallagher <john@synapticmishap.co.uk>
2014-12-14 21:09:01 +0000

    Render flash messages properly.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def alert_class(type)
+    if type.to_sym == :alert
+      'alert-warning'
+    elsif type.to_sym == :notice
+      'alert-info'
+    elsif type.to_sym == :error
+      'alert-danger'
+    end
+  end
 end
