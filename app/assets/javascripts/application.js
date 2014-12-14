5382cb8fbdc9277662e9288515f40517bb28c1ec
John Gallagher <john@synapticmishap.co.uk>
2014-12-14 23:26:55 +0000

    Use date range picker.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap/dist/js/bootstrap
+//= require moment/moment
+//= require bootstrap-daterangepicker/daterangepicker
 //= require_tree .
