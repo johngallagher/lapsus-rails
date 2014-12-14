93ffff340deabe78eb9ef9dd767022725ba94e1e
John Gallagher <john@synapticmishap.co.uk>
2014-12-14 23:26:44 +0000

    Fix issue when an entry has a nil project.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,7 +5,7 @@ class ReportsController < ApplicationController
     todays_entries = Entry.for_user(current_user).where('started_at > ? and finished_at < ?', 1.days.ago, Time.now)
     grouped_entries = todays_entries.group_by(&:project_id)
     @report = grouped_entries.map do |project_id, entries|
-      OpenStruct.new(name: Project.find(project_id).name, time: entries.map(&:duration).inject(&:+))
+      OpenStruct.new(name: entries.first.project_name, time: entries.map(&:duration).inject(&:+))
     end
   end
 end
