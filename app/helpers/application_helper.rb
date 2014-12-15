9add12569a411f086049879f8c0f1d4833071dcd
John Gallagher <john@synapticmishap.co.uk>
2014-12-15 14:54:53 +0000

    Added a helper to format time and backfilled tests for alert class.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,17 @@ module ApplicationHelper
   def alert_class(type)
     if type.to_sym == :alert
       'alert-warning'
-    elsif type.to_sym == :notice
-      'alert-info'
     elsif type.to_sym == :error
       'alert-danger'
+    else
+      'alert-info'
     end
+  end
+
+  def as_hours_and_minutes(total_seconds)
+    seconds = total_seconds % 60
+    minutes = (total_seconds / 60) % 60
+    hours = total_seconds / (60 * 60)
+    format('%d:%02d', hours, minutes)
   end
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+  include ApplicationHelper
+
+  it 'adds bootstrap classes to flash classes' do
+    expect(alert_class(:alert)).to eq('alert-warning')
+    expect(alert_class(:notice)).to eq('alert-info')
+    expect(alert_class(:error)).to eq('alert-danger')
+    expect(alert_class(:something_else)).to eq('alert-info')
+  end
+
+  it 'formats durations' do
+    expect(as_hours_and_minutes(60)).to eq('0:01')
+    expect(as_hours_and_minutes(61)).to eq('0:01')
+    expect(as_hours_and_minutes(119)).to eq('0:01')
+    expect(as_hours_and_minutes(120)).to eq('0:02')
+    expect(as_hours_and_minutes(3600)).to eq('1:00')
+  end
+
+end
