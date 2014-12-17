5b1d3053439d815e8efc9574841450a0f60ba885
John Gallagher <john@synapticmishap.co.uk>
2014-12-17 22:07:47 +0000

    Eagerly load projects - remove crazy numbers of queries.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,6 +2,6 @@ class EntriesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @entries = Entry.for_user(current_user).order('started_at DESC')
+    @entries = Entry.for_user(current_user).includes(:project).order('started_at DESC')
   end
 end
