ac9ce73f09acf6cb59a00b9d375f3f28ed308cc6
John Gallagher <john@synapticmishap.co.uk>
2014-12-17 22:12:10 +0000

    Find a project with a preset false so it doesn't accidentally set no project.

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,11 +3,11 @@ class Project < ActiveRecord::Base
   scope :for_user, lambda { |user| where(user_id: user.id) }
 
   def self.preset_with_name(name)
-    Project.create(name: name, preset: true, url: nil)
+    Project.create(name: name, preset: true)
   end
 
   def self.find_or_create_from_container_and_entry(container, entry)
-    find_or_create_by(path: container.project_path_from_entry(entry)) do |project|
+    find_or_create_by(path: container.project_path_from_entry(entry), preset: false) do |project|
       project.name = container.project_name_from_entry(entry)
       project.user_id = container.user_id
     end
