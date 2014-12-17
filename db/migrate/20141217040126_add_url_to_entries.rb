524978a4e626ea86207cf57ea9a99b275c250e03
John Gallagher <john@synapticmishap.co.uk>
2014-12-17 04:54:50 +0000

    Migrate all paths across to urls.

--- db/migrate/20141217040126_add_url_to_entries.rb
+++ db/migrate/20141217040126_add_url_to_entries.rb
@@ -1,5 +1,15 @@
 class AddUrlToEntries < ActiveRecord::Migration
-  def change
+  def up
     add_column :entries, :url, :string
+    Entry.all.each do |entry|
+      if entry.attributes[:path].present?
+        entry.url = "file://#{entry.attributes[:path]}" 
+        entry.save!
+      end
+    end
+  end
+
+  def down
+    remove_column :entries, :url, :string
   end
 end
